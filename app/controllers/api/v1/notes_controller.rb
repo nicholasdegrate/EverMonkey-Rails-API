cc91a2144f4ec85a572650d5d1cee44c35b6bd6d
Jeffrey Platt <JSPlatt@gmail.com>
2021-03-28 15:25:37 -0400

    note backend started

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -4,4 +4,36 @@ class Api::V1::NotesController < Api::V1::BaseController
         notes = Note.all
         render json: serialize_models(notes), status: :ok
     end
+
+    def show
+        note = Note.find(params[:id])
+        render json: serialize_model(note), status: :ok
+    end
+
+    def create
+
+        note = Note.create(note_params)
+
+        if note.save
+            render json: serialize_model(note)
+        else
+            render json: serialize_model_error(note.errors)
+        end
+    end
+
+    def update
+        note = Note.find(params[:id])
+        render json: serialize_model(note), status: :ok   
+    end
+
+    def destroy
+        note = Note.find(params[:id])
+        note.destroy
+    end
+
+    private
+
+        def note_params
+            params.require(:note).permit(:name, :paragraph, :note_book_id, :delete_object)
+        end
 end
